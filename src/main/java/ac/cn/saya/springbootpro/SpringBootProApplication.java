66e378cfdc52fce97996414e6362f0e815fc6dce
742442849@qq.com <Lnk742442849>
2019-01-11 00:21:59 +0800

    增加日志打印

--- src/main/java/ac/cn/saya/springbootpro/SpringBootProApplication.java
+++ src/main/java/ac/cn/saya/springbootpro/SpringBootProApplication.java
@@ -1,13 +1,19 @@
 package ac.cn.saya.springbootpro;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootProApplication {
 
+	private static Logger logger = Logger.getLogger(SpringBootProApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProApplication.class, args);
+		//项目启动完成打印项目名
+		logger.error("服务已经启动 ... ");
+		logger.warn("服务已经启动 ... ");
 	}
 
 }
