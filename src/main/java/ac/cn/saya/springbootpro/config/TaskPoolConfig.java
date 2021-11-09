24f5e2b9427300c0ccc9823512722580a73ce850
liunengkai <saya@saya.ac.cn>
2021-11-09 21:54:27 +0800

    增加异步编程示例

---
+++ src/main/java/ac/cn/saya/springbootpro/config/TaskPoolConfig.java
@@ -0,0 +1,47 @@
+package ac.cn.saya.springbootpro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @Title: TaskPoolConfig
+ * @ProjectName spring-boot-pro
+ * @Description: TODO
+ * @Author liunengkai
+ * @Date: 11/9/21 11:01
+ * @Description:
+ */
+
+@EnableAsync
+@Configuration
+public class TaskPoolConfig {
+
+    @Bean
+    public Executor taskExecutor1() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(10);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("executor-1-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        return executor;
+    }
+
+    @Bean
+    public Executor taskExecutor2() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(10);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("executor-2-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        return executor;
+    }
+}
